28097b6234945585b6c15196e5bfffed3d882efc
Dixon <dixonwillow23@gmail.com>
2023-10-06 19:43:08 +0700

    fix: move tailwind into folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+export default {
+  content: [],
   theme: {
     extend: {},
   },

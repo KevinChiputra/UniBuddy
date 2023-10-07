319e2eea56568c7dd35e9425f7eddd93f499b783
Dixon <dixonwillow23@gmail.com>
2023-10-07 19:53:16 +0700

    config: font and color tailwind setup

--- UniBuddy/tailwind.config.js
+++ UniBuddy/tailwind.config.js
@@ -6,6 +6,19 @@ export default {
   ],
   theme: {
     extend: {},
+
+    colors: {
+      'primary' : '#CF5841',
+      'nw200' : '#E7EAED',
+      'nw300' : '#707070',
+      'nb100' : '1B1B1B',
+      'transgm' : '#505050'
+
+    },
+
+    fontFamily: {
+      'pjs' : ['Plus Jakarta Sans', 'sans-serif']
+    }
   },
   plugins: [],
 }

cf894d5799f7557d423117a66b7a9e7e0e562be3
Dixon <dixonwillow23@gmail.com>
2023-10-07 20:28:23 +0700

    fix: fix color tailwind config

--- UniBuddy/tailwind.config.js
+++ UniBuddy/tailwind.config.js
@@ -5,15 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
-
-    colors: {
-      'primary' : '#CF5841',
-      'nw200' : '#E7EAED',
-      'nw300' : '#707070',
-      'nb100' : '1B1B1B',
-      'transgm' : '#505050'
-
+    extend: {
+      colors: {
+        'primary' : '#CF5841',
+        'nw200' : '#E7EAED',
+        'nw300' : '#707070',
+        'nb100' : '1B1B1B',
+        'transgm' : '#505050'
+  
+      },
+      
     },
 
     fontFamily: {
